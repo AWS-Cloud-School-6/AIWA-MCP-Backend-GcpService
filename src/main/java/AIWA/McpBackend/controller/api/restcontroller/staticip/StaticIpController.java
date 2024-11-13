d052d608d1a5c397dc758bba71d05abdb4ff3877
nareunhang <xuwrgaf@gmail.com>
2024-11-13 14:23:46 +0900

    #4 Feat: Add StaticIp Describe

--- src/main/java/AIWA/McpBackend/controller/api/restcontroller/staticip/StaticIpController.java
+++ src/main/java/AIWA/McpBackend/controller/api/restcontroller/staticip/StaticIpController.java
@@ -1,26 +1,26 @@
-//package AIWA.McpBackend.controller.api.restcontroller.eip;
-//
-//import AIWA.McpBackend.controller.api.dto.eip.EipDto;
-//import AIWA.McpBackend.controller.api.dto.eip.EipRequestDto;
-//import AIWA.McpBackend.controller.api.dto.response.CommonResult;
-//import AIWA.McpBackend.controller.api.dto.response.ListResult;
-//import AIWA.McpBackend.service.gcp.GcpResourceService;
+package AIWA.McpBackend.controller.api.restcontroller.staticip;
+
+import AIWA.McpBackend.controller.api.dto.response.ListResult;
+import AIWA.McpBackend.controller.api.dto.staticip.StaticIpDto;
+import AIWA.McpBackend.service.gcp.GcpResourceService;
 //import AIWA.McpBackend.service.gcp.eip.EipService;
-//import AIWA.McpBackend.service.response.ResponseService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/aws/api/eip")
-//@RequiredArgsConstructor
-//public class EipController {
-//
+import AIWA.McpBackend.service.response.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gcp/api/eip")
+@RequiredArgsConstructor
+public class StaticIpController {
+
 //    private final EipService eipService;
-//    private final GcpResourceService gcpResourceService;
-//    private final ResponseService responseService;
-//
+    @Autowired
+    private final GcpResourceService gcpResourceService;
+    private final ResponseService responseService;
+
 //    /**
 //     * EIP 생성 엔드포인트
 //     *
@@ -54,10 +54,15 @@
 //            return responseService.getFailResult();
 //        }
 //    }
-//
-//    @GetMapping("/describe")
-//    public ListResult<EipDto> describeEip(@RequestParam String userId) {
-//        List<EipDto> eips = gcpResourceService.fetchElasticIps(userId);
-//        return responseService.getListResult(eips);
-//    }
-//}
+
+    @GetMapping("/describe")
+    public ListResult<StaticIpDto> getStaticIps(
+            @RequestParam String projectId) {  // projectId 파라미터 받기
+
+        // gcpResourceService.getStaticIpsFromGCP에서 반환된 List<StaticIpInfoDTO>를 가져옵니다.
+        List<StaticIpDto> staticIps = gcpResourceService.getStaticIpsFromGCP(projectId);
+
+        // List<StaticIpInfoDTO>를 ResponseService의 getListResult를 사용하여 감싸서 반환합니다.
+        return responseService.getListResult(staticIps);
+    }
+}
