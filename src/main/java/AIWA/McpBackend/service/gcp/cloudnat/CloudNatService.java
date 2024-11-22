c40b4cd33e581a386df34a3fdc2d4bd2b5af8d56
nareunhang <xuwrgaf@gmail.com>
2024-11-22 15:29:58 +0900

    #30 Feat: Cloud Router 생성 추가

--- src/main/java/AIWA/McpBackend/service/gcp/cloudnat/CloudNatService.java
+++ src/main/java/AIWA/McpBackend/service/gcp/cloudnat/CloudNatService.java
@@ -1,7 +1,6 @@
 package AIWA.McpBackend.service.gcp.cloudnat;
 
 import AIWA.McpBackend.controller.api.dto.cloudnat.CloudNatRequestDto;
-import AIWA.McpBackend.controller.api.dto.natgateway.NatGatewayRequestDto;
 import AIWA.McpBackend.service.terraform.TerraformService;
 import AIWA.McpBackend.service.gcp.s3.S3Service;
 import lombok.RequiredArgsConstructor;
@@ -64,49 +63,19 @@ public class CloudNatService {
     /**
      * NAT Gateway를 삭제합니다.
      *
-     * @param natGatewayName NAT Gateway 이름
+     * @param cloudNatName NAT Gateway 이름
      * @param userId         사용자 ID
      * @throws Exception NAT Gateway 삭제 중 발생한 예외
      */
-    public void deleteCloudNat(String natGatewayName, String userId) throws Exception {
-        // 1. 삭제하려는 NAT Gateway .tf 파일 이름 설정 (예: nat_gateway_myNatGateway.tf)
-        String natGatewayTfFileName = String.format("nat_gateway_%s.tf", natGatewayName);
+    public void deleteCloudNat(String cloudNatName, String userId) throws Exception {
+        // 1. 삭제하려는 NAT Gateway .tf 파일 이름 설정 (예: nat_gateway_myCloudNat.tf)
+        String cloudNatTfFileName = String.format("cloud_nat_%s.tf", cloudNatName);
 
         // 2. S3에서 해당 NAT Gateway .tf 파일 삭제
-        String s3Key = "users/" + userId + "/" + natGatewayTfFileName;
+        String s3Key = "users/" + userId + "/GCP/" + cloudNatTfFileName;
         s3Service.deleteFile(s3Key);
 
         // 3. Terraform 실행 요청
-        terraformService.executeTerraform(userId);
-    }
-
-    /**
-     * NAT Gateway로 라우트를 추가합니다.
-     *
-     * @param routeTableName Route Table 이름
-     * @param natGatewayName NAT Gateway 이름
-     * @param cidrBlock      CIDR 블록 (예: "0.0.0.0/0"은 모든 트래픽)
-     * @param userId         사용자 ID
-     * @throws Exception NAT Gateway로의 라우트 추가 중 발생한 예외
-     */
-    public void addRouteToNatGateway(String routeTableName, String natGatewayName, String cidrBlock, String userId) throws Exception {
-        // 1. Route를 추가하는 코드 블록 생성
-        String routeTfContent = String.format("""
-                resource "aws_route" "route_to_nat" {
-                  route_table_id = aws_route_table.%s.id
-                  destination_cidr_block = "%s"
-                  nat_gateway_id = aws_nat_gateway.%s.id
-                }
-                """, routeTableName, cidrBlock, natGatewayName);
-
-        // 2. Route .tf 파일 이름 설정 (예: route_to_nat_gateway.tf)
-        String routeTfFileName = String.format("route_%s_to_nat_gateway.tf", routeTableName);
-
-        // 3. S3에 새로운 Route .tf 파일 업로드
-        String s3Key = "users/" + userId + "/" + routeTfFileName;
-        s3Service.uploadFileContent(s3Key, routeTfContent);
-
-        // 4. Terraform 실행 요청
         terraformService.executeTerraform(userId);
     }
 }
--- src/main/java/AIWA/McpBackend/service/gcp/cloudrouter/CloudRouterService.java
+++ src/main/java/AIWA/McpBackend/service/gcp/cloudrouter/CloudRouterService.java
@@ -1,63 +1,61 @@
-//package AIWA.McpBackend.service.gcp.cloudrouter;
-//
-//import AIWA.McpBackend.controller.api.dto.internetgateway.InternetGatewayRequestDto;
-//import AIWA.McpBackend.service.terraform.TerraformService;
-//import AIWA.McpBackend.service.gcp.s3.S3Service;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CloudRouterService {
-//
-//    private final S3Service s3Service;
-//    private final TerraformService terraformService;
-//
-//    /**
-//     * Internet Gateway를 생성합니다.
-//     *
-//     * @param igwRequest Internet Gateway 생성 요청 DTO
-//     * @param userId     사용자 ID
-//     * @throws Exception Internet Gateway 생성 중 발생한 예외
-//     */
-//    public void createInternetGateway(InternetGatewayRequestDto igwRequest, String userId) throws Exception {
-//        // 1. 새로운 Internet Gateway .tf 파일 생성
-//        String igwTfContent = String.format("""
-//                resource "aws_internet_gateway" "%s" {
-//                  vpc_id = aws_vpc.%s.id
-//                  tags = {
-//                    Name = "%s"
-//                  }
-//                }
-//                """, igwRequest.getIgwName(), igwRequest.getVpcName(), igwRequest.getIgwName());
-//
-//        // 2. Internet Gateway .tf 파일 이름 설정 (예: igw_myInternetGateway.tf)
-//        String igwTfFileName = String.format("igw_%s.tf", igwRequest.getIgwName());
-//
-//        // 3. S3에 새로운 Internet Gateway .tf 파일 업로드
-//        String s3Key = "users/" + userId + "/" + igwTfFileName;
-//        s3Service.uploadFileContent(s3Key, igwTfContent);
-//
-//        // 4. Terraform 실행 요청
-//        terraformService.executeTerraform(userId);
-//    }
-//
-//    /**
-//     * Internet Gateway를 삭제합니다.
-//     *
-//     * @param igwName Internet Gateway 이름
-//     * @param userId  사용자 ID
-//     * @throws Exception Internet Gateway 삭제 중 발생한 예외
-//     */
-//    public void deleteInternetGateway(String igwName, String userId) throws Exception {
-//        // 1. 삭제하려는 Internet Gateway .tf 파일 이름 설정 (예: igw_myInternetGateway.tf)
-//        String igwTfFileName = String.format("igw_%s.tf", igwName);
-//
-//        // 2. S3에서 해당 Internet Gateway .tf 파일 삭제
-//        String s3Key = "users/" + userId + "/" + igwTfFileName;
-//        s3Service.deleteFile(s3Key);
-//
-//        // 3. Terraform 실행 요청
-//        terraformService.executeTerraform(userId);
-//    }
-//}
+package AIWA.McpBackend.service.gcp.cloudrouter;
+
+import AIWA.McpBackend.controller.api.dto.cloudrouter.CloudRouterRequestDto;
+import AIWA.McpBackend.controller.api.dto.internetgateway.InternetGatewayRequestDto;
+import AIWA.McpBackend.service.terraform.TerraformService;
+import AIWA.McpBackend.service.gcp.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CloudRouterService {
+
+    private final S3Service s3Service;
+    private final TerraformService terraformService;
+
+    /**
+     * Internet Gateway를 생성합니다.
+     *
+     * @param cloudRouterRequest Internet Gateway 생성 요청 DTO
+     * @param userId     사용자 ID
+     * @throws Exception Internet Gateway 생성 중 발생한 예외
+     */
+    public void createCloudRouter(CloudRouterRequestDto cloudRouterRequest, String userId) throws Exception {
+        // 1. 새로운 Cloud Router .tf 파일 생성
+        String cloudRouterTfContent = String.format("""
+            resource "google_compute_router" "%s" {
+              name    = "%s"
+              region  = "%s"
+              network = "%s"
+              description = "%s"
+            }
+            """, cloudRouterRequest.getRouterName(), cloudRouterRequest.getRouterName(),
+                cloudRouterRequest.getRegion(), cloudRouterRequest.getNetwork(), cloudRouterRequest.getDescription());
+
+        // 2. Cloud Router .tf 파일 이름 설정 (예: cloud_router_myRouter.tf)
+        String cloudRouterTfFileName = String.format("cloud_router_%s.tf", cloudRouterRequest.getRouterName());
+
+        // 3. 콘솔에 내용 출력 (디버깅 용도)
+        System.out.println(cloudRouterTfContent);
+
+        // 4. GCS에 새로운 Cloud Router .tf 파일 업로드
+        String gcsKey = "users/" + userId + "/GCP/" + cloudRouterTfFileName;
+        s3Service.uploadFileContent(gcsKey, cloudRouterTfContent);
+
+        // 5. Terraform 실행 요청
+        terraformService.executeTerraform(userId);
+    }
+
+    public void deleteCloudRouter(String routerName, String userId) throws Exception {
+        // 1. 삭제하려는 Cloud Router .tf 파일 이름 설정 (예: cloud_router_myRouter.tf)
+        String cloudRouterTfFileName = String.format("cloud_router_%s.tf", routerName);
+
+        // 2. GCS에서 해당 Cloud Router .tf 파일 삭제
+        String gcsKey = "users/" + userId + "/GCP/" + cloudRouterTfFileName;
+        s3Service.deleteFile(gcsKey);
+
+        // 3. Terraform 실행 요청
+        terraformService.executeTerraform(userId);
+    }
+}
